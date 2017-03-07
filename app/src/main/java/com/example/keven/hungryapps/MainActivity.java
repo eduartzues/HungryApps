5ed91ed86d004309abd29905dbcd96b027561d3c
Keven <eduartzuessm@yahoo.com>
2017-03-07 10:45:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/keven/hungryapps/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.keven.hungryapps;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+
+
+
+    public void eatCookie(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        ImageView image = (ImageView) findViewById(R.id.android_cookie_image_view);
+        image.setImageResource(R.drawable.after);
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+        TextView text = (TextView) findViewById(R.id.status_text_view);
+        text.setText("I'm FULL!");
+
+    }
+}
